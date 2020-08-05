d18ad783cf795e4ed6a4c4292371804f606a6dcf
Lashonda Miller <lashondamiller007@gmail.com>
2020-08-05 15:49:38 -0400

    Updates

---
+++ precache-manifest.6bdd4a23007ff3eb477830d9fca2ed47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8297ec0887fa0876ab4e62c8e8cf02",
+    "url": "/starrtimes/index.html"
+  },
+  {
+    "revision": "80443d7feb7bf66dbceb",
+    "url": "/starrtimes/static/css/main.863f997b.chunk.css"
+  },
+  {
+    "revision": "598d1b896dcacc9f96b4",
+    "url": "/starrtimes/static/js/2.0572871d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starrtimes/static/js/2.0572871d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80443d7feb7bf66dbceb",
+    "url": "/starrtimes/static/js/main.89ece697.chunk.js"
+  },
+  {
+    "revision": "1b738fb3775901107ba1",
+    "url": "/starrtimes/static/js/runtime-main.b46e3bae.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/starrtimes/static/media/pokeball.f10bf078.png"
+  }
+]);
